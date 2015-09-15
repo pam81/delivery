56233496ac15426b1ee511b4eedd80d7c71288fa
Belen <belen.alegre@gmail.com>
2015-09-15 00:25:58 -0300

    Registro
    Algunas partes de esto

--- src/Frontend/HomeBundle/Controller/HomeController.php
+++ src/Frontend/HomeBundle/Controller/HomeController.php
@@ -137,6 +137,178 @@ class HomeController extends Controller
     
     }
     
+    public function checkBarrioAction(Request $request){
+     
+      $barrio=trim(mb_convert_case($request->get("barrio"),MB_CASE_LOWER));
+      $zona=trim(mb_convert_case($request->get("zona"),MB_CASE_LOWER));
+      
+      $dql="SELECT u FROM BackendAdminBundle:Zona u "  ;
+      $dql.=" where u.name like '".$zona."' ";
+      $dql .=" order by u.name";
+      
+      //primero localizo la zona luego localizo el barrio
+      //si no existe la zona ni el barrio muestro todo
+      //sino muestro segun lo que pueda identificar
+       $em = $this->getDoctrine()->getManager();
+       $query = $em->createQuery($dql);
+       $zonas = $query->getResult();
+       $zonaId = 0;
+       $barrioId = 0;
+       if (count($zonas) == 1){
+          $zonaId = $zonas[0]->getId();
+          $dql="SELECT u FROM BackendAdminBundle:Barrio u "  ;
+          $dql.=" where u.name like '".$barrio."' and u.zona ='".$zonaId."' ";
+          $dql .=" order by u.name";
+          
+          $query = $em->createQuery($dql);
+          $barrios = $query->getResult();
+          if (count($barrios) == 1){
+              $barrioId = $barrios[0]->getId();
+          }
+       }
+        $resultado=array("zonaId"=>$zonaId, "barrioId"=>$barrioId);
+      
+        $response = new Response(json_encode($resultado));
+        
+       $response->headers->set('Content-Type', 'application/json');
+  
+       return $response;
+       
+    
+    }
+    /*
+	public function loginCreateAction(){
+		
+		return $this->render('FrontendHomeBundle:Login:index.html.twig');
+		
+	}
+    */
+    public function loginAction(Request $request){
+    
+        $email=$request->get("email");
+        $password=$request->get("password");
+        
+        $path=$this->generateUrl("frontend_homepage");
+        
+        if ($request->get("_target_path")){
+          $path=$this->generateUrl($request->get("_target_path"));
+        }
+        $resultado=array("status"=>0,"message"=>'', "redirect"=>'');
+        if ($email && $password){
+        try{
+          
+          $em = $this->getDoctrine()->getManager();
+          $user=$em->getRepository('BackendCustomerBundle:Customer')->loadUserByUsername($email);
+          
+          if ( null !== $user && $user->comparePassword($password) ){
+                  
+                if (!$user->getIsActive()){
+                     
+                     $resultado["message"]='Usuario inhabilitado.';
+                     $resultado["status"]=0;
+                }else{  
+                  $this->get('session')->set("user",$user);
+                  $token = new UsernamePasswordToken($user, null, "frontend", $user->getRoles());
+                  $this->get("security.context")->setToken($token); //now the user is logged in
+                  $resultado["message"]='Usuario válido.';
+                  $resultado["status"]=1;
+                  $resultado["redirect"]=$path;
+                  
+                  $resultado["user"]=array("email"=>$user->getEmail(), "name"=>$user->getName(),"token"=>$token);
+               }   
+                  
+          }else{
+                  $resultado["message"]='Usuario y/o clave incorrectas.';
+                  $resultado["status"]=0;             
+                
+          
+          } 
+           
+         }catch(Exception $e){
+                 $resultado["message"]='Usuario y/o clave incorrectas.';
+                 $resultado["status"]=0; 
+                
+         }
+         
+        }else{
+          
+              $resultado["message"]='Usuario y/o clave incorrectas.';
+              $resultado["status"]=0;
+              
+        }
+        
+        $response = new Response(json_encode($resultado));
+        
+       $response->headers->set('Content-Type', 'application/json');
+  
+       return $response;
+    }
+	
+	
+
+    public function registerAction(Request $request)
+    {
+       if ($request->getMethod() == 'POST') {  
+        
+        $customer=array();
+        $customer["email"] = $this->getRequest()->get("email", null);    
+    	$customer["password"] = $this->getRequest()->get("password", null);
+    	$customer["name"] = $this->getRequest()->get("name", null);
+        $customer["lastname"] = $this->getRequest()->get("lastname", null);
+        $customer["terminos"] = $this->getRequest()->get("terminos", null);
+		
+		
+		if($this->getRequest()->get("comercio", null) == 1){
+		
+        	$customer["role"]="ROLE_VENDEDOR";
+        
+		}else{
+        	
+			$customer["role"]="ROLE_VISITOR";			
+        }        
+        
+    		$service = new \Backend\CustomerBundle\Services\CustomerService($this->get('doctrine.orm.default_entity_manager'));
+    		$registerResponse = $service->register($cliente);
+    		
+    		$respuesta=json_decode($registerResponse);
+    		
+    		if ($respuesta->status == 0) //se creo el cliente envio mail
+    		{
+    		  $em = $this->getDoctrine()->getManager();
+          	  $empresa = $em->getRepository('BackendCustomerBundle:Seteo')->findOneByName("empresa");
+    		  $email_site = $em->getRepository('BackendCustomerBundle:Seteo')->findOneByName("email");
+    		  
+    		  $url= $this->generateUrl(
+            'activate_account',
+            array('codigo' =>$respuesta->codigo ), true );
+    		  
+    		  $message = \Swift_Message::newInstance()
+                    ->setSubject("Registro en ".$empresa->getValue())
+                    ->setFrom($email_site->getValue())
+                    ->setTo($customer["email"])
+                    ->setBody(
+                        $this->renderView(
+                            'FrontendHomeBundle:Registro:register_email.html.twig',
+                            array('name' => $customer["name"],
+                             'url' =>$url
+                             )
+                        ),'text/html'
+                    );
+        
+        
+         
+          @$this->get('mailer')->send($message);
+          $this->get('session')->getFlashBag()->add('success' , $respuesta->message);
+    		}else{
+           $this->get('session')->getFlashBag()->add('error' , $respuesta->message);
+         }
+    		
+       }
+            return $this->render('FrontendHomeBundle:Registro:registrarse.html.twig');   
+        
+        
+    }
+		
     
 } 
  
