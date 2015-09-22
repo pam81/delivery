ea091fd69e166cea9f041b1e7dbba0df30e9ac88
belen <belen.alegre@gmail.com>
2015-09-21 23:10:08 -0300

    compra

--- src/Backend/CustomerAdminBundle/Resources/public/js/validate_sucursal.js
+++ src/Backend/CustomerAdminBundle/Resources/public/js/validate_sucursal.js
@@ -2,18 +2,18 @@ $(document).ready(function() {
 		var validator = $("#tab").validate({
 		
 			rules: {                 
-				"backend_customeradminbundle_sucursal[name]": {
+				"backend_customeradminbundle_sucursaltype[name]": {
 					required:true,
 					minlength:2,
 					maxlength:100,
 				}
-				"backend_customeradminbundle_sucursal[cuit]": {
+				"backend_customeradminbundle_sucursaltype[cuit]": {
 					required:true,
 					numeric:true,
 					minlength:11,
 					maxlength:20,
 				}
-				"backend_customeradminbundle_sucursal[phone]": {
+				"backend_customeradminbundle_sucursaltype[phone]": {
 					required:true,
 					minlength:8,
 					maxlength:20,
@@ -21,20 +21,20 @@ $(document).ready(function() {
 			},
 			
 		    messages: {
-            "backend_customeradminbundle_sucursal[name]": {
+            "backend_customeradminbundle_sucursaltype[name]": {
             required: "Ingrese el nombre de la sucursal",
             maxlength: jQuery.format("Máximo {0} carácteres!"),
             minlength: jQuery.format("Mínimo {0} carácteres!")
             },
 		 	messages: {
-           "backend_customeradminbundle_sucursal[cuit]": {
+           "backend_customeradminbundle_sucursaltype[cuit]": {
            required: "Ingrese el numero de cuit",
 		   numeric: "Ingrese los numeros sin guiones",	   
            maxlength: jQuery.format("Máximo {0} carácteres!"),
            minlength: jQuery.format("Mínimo {0} carácteres!")
            },
 		 	messages: {
-           "backend_customeradminbundle_sucursal[telefono]": {
+           "backend_customeradminbundle_sucursaltype[telefono]": {
            required: "Ingrese el numero de telefono",
            maxlength: jQuery.format("Máximo {0} carácteres!"),
            minlength: jQuery.format("Mínimo {0} carácteres!")
