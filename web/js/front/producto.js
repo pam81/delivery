78c6db2afa2cac4429aafa47af11c0590c42ac6f
pam81 <papereyra@gmail.com>
2015-10-28 21:08:35 -0300

    agregar variedades en el carrito enviar mails agregar aclaraciones al pedido

--- web/js/front/producto.js
+++ web/js/front/producto.js
@@ -29,14 +29,27 @@ function loadCantidad(){
 
 //si el producto que muestro de la tienda esta en el 
    //carrito le cargo la cantidad que esta comprando
+   var listado=new Array();
+   
    simpleCart.each( function( item ){
       var id= item.get("product_id");
-       console.log(id);
-     if ( $("#input-producto-"+id) ){
-          $("#input-producto-"+id).val(item.quantity());
-     }
+      var cantidad=0;
+      if (listado[id]){
+         cantidad = listado[id]+item.quantity();
+         listado[id]= cantidad;
+      }else{
+         listado[id]=item.quantity();
+      }
+     
    
    
    });
+   
+  for (var key in listado ) {
+        if ( $("#input-producto-"+key) ){
+          $("#input-producto-"+key).val(listado[key]);
+     }
+   }
+   
 
 }
